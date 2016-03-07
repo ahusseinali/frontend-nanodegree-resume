a17727b40ddeb9abd73ed8ef910bd63beb5599a8
Ahmed Ali <a.husseinzaki@gmail.com>
2016-03-07 15:37:54 -0800

    feat: Add biography information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,27 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Ahmed Ali",
+    "role": "Software Engineer",
+    "contactInfo": {
+        "gmail": "a.husseinzaki@gmail.com",
+        "hotmail": "a.husseinzaki@hotmail.com",
+        "github": "https://github.com/ahusseinali",
+        "mobile": "+1(425)615-5422"
+    },
+    "pictureUrl": "https://avatars0.githubusercontent.com/u/9116873?v=3&s=460",
+    "welcomeMessage": "Welcome to my personal resume page.",
+    "skills": ["Software Development", "Software Design", "Web Development",
+        "RESTful services", ".NET", "Java", "HTML", "CSS", "JavaScript",
+        "Bootstrap", "KnockoutJS"]
+};
+
+var dataReplace = "%data%";
+$("#header").prepend(HTMLheaderRole.replace(dataReplace, bio.role));
+$("#header").prepend(HTMLheaderName.replace(dataReplace, bio.name));
+$("#header").prepend(HTMLbioPic.replace(dataReplace, bio.pictureUrl));
+$("#topContacts").append(HTMLmobile.replace(dataReplace, bio.contactInfo.mobile));
+$("#topContacts").append(HTMLemail.replace(dataReplace, bio.contactInfo.gmail));
+$("#topContacts").append(HTMLemail.replace(dataReplace, bio.contactInfo.hotmail));
+$("#topContacts").append(HTMLgithub.replace(dataReplace, bio.contactInfo.github));
+$("#header").append(HTMLwelcomeMsg.replace(dataReplace, bio.welcomeMessage));
+$("#header").append(HTMLskillsStart);
+$("#skills").append(HTMLskills.replace(dataReplace, bio.skills.join(", ")));
