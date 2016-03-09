f2dadd3d5f485b9400692952d1f8bc3597b515f9
Ahmed Ali <a.husseinzaki@gmail.com>
2016-03-08 21:39:01 -0800

    bug: Fix spacing between words in descriptions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,30 +19,30 @@ var work = [{
     'employer': 'Microsoft Corp.',
     'years': 'October 2013 - present',
     'city': 'Redmond, WA, USA',
-    'description': 'I am a Software Engineer at Microsoft. I work on the services side in' +
-        'Bing Online Advertising. I am responsible for improving and maintaining offline pipeline' +
-        'services that extract features from raw BI logs to help with predicting click probability' +
-        'on user queries. My role extensively uses .Net framework along with internal frameworks' +
-        'and tools to handle Big Data management and job scheduling. I work on performance,' +
-        'scale and validation problems to maintain SLA with the increasing scale of data'
+    'description': 'I am a Software Engineer at Microsoft. I work on the services side in ' +
+        'Bing Online Advertising. I am responsible for improving and maintaining offline pipeline ' +
+        'services that extract features from raw BI logs to help with predicting click probability ' +
+        'on user queries. My role extensively uses .Net framework along with internal frameworks ' +
+        'and tools to handle Big Data management and job scheduling. I work on performance, ' +
+        'scale and validation problems to maintain SLA with the increasing scale of data.'
 },{
     'position': 'Software Development Contestant',
     'employer': 'Freelance at TopCoder (http://www.topcoder.com)',
     'years': 'April 2012 - September 2013',
     'city': 'Home Office',
-    'description': 'I participated and won at several development contests that were held by' +
-        'TopCoder community. My Profile (https://www.topcoder.com/members/DrAhmed) has a detailed' +
-        'description of all the projects I participated in. I used ASP.NET MVC 3.0, C#.NET 4.0, WCF,' +
-        'Html, jQuery, IBM Lotus Notes, MS Access 2007 with VBA, SQL Server 2008 and Linux Shell script' +
+    'description': 'I participated and won at several development contests that were held by ' +
+        'TopCoder community. My Profile (https://www.topcoder.com/members/DrAhmed) has a detailed ' +
+        'description of all the projects I participated in. I used ASP.NET MVC 3.0, C#.NET 4.0, WCF, ' +
+        'Html, jQuery, IBM Lotus Notes, MS Access 2007 with VBA, SQL Server 2008 and Linux Shell script ' +
         'in most of the contests.'
 },{
     'position': 'Teaching Assistant',
     'employer': 'Faculty of Engineering, Cairo University',
     'years': 'March 2011 - September 2013',
     'city': 'Giza, Egypt',
-    'description': 'I Worked as a teaching assistant giving lab sessions and tutorials in' +
-        'some computer science concepts such as Fundamentals of operating systems,' +
-        'Distributed Operating Systems, Logic Design, Computer Networks, Computer Graphics' +
+    'description': 'I Worked as a teaching assistant giving lab sessions and tutorials in ' +
+        'some computer science concepts such as Fundamentals of operating systems, ' +
+        'Distributed Operating Systems, Logic Design, Computer Networks, Computer Graphics ' +
         'and Database Administration'
 }];
 
